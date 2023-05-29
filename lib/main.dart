2ed430617eafc93b9745d7c6ce53769328fca92a
ynrrani <yaniar.rani12@gmail.com>
2023-05-29 22:38:38 +0700

    feat: nyicil backend, but we got red(gon fix asap)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tikum_mobile/controllers/popular_product_controller.dart';
 import 'package:tikum_mobile/pages/HomePage.dart';
 import 'package:tikum_mobile/pages/food/popular_food_detail.dart';
 import 'package:tikum_mobile/pages/food/recommended_food_detail.dart';
@@ -8,11 +9,18 @@ import 'package:tikum_mobile/pages/ItemPage.dart';
 import 'package:get/get.dart';
 import 'package:tikum_mobile/pages/profile/login.dart';
 import 'package:tikum_mobile/pages/profile/register.dart';
-void main() => runApp(MyApp());
+import 'helper/dependencies.dart' as dep;
+
+Future<void> main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+      Get.find<PopularProductController>().getPopularProductList();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
